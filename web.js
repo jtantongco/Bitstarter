0b24bd751ff74862b66f8991d8e61f15d5a0e594
Jeremiah Tantongco <jtantongco@gmail.com>
2013-07-11 01:42:46 +0000

    Rolling forward

--- web.js
+++ web.js
@@ -3,10 +3,10 @@ var express = require('express');
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
- // var fs = require('fs');
- // var buffer = fs.readFileSync('index.html');
- // response.send(buffer.toString());
- response.send("Hello World, Ugh.");
+  var fs = require('fs');
+  var buffer = fs.readFileSync('index.html');
+  response.send(buffer.toString());
+ //response.send("Hello World, Ugh.");
 });
 
 var port = process.env.PORT || 5000;
