48d812678923b997d365922eeddbbebfddb4b557
Ubuntu <ubuntu@ip-172-31-41-83.us-west-2.compute.internal>
2013-07-11 01:38:54 +0000

    Rolling it back

--- web.js
+++ web.js
@@ -3,8 +3,10 @@ var express = require('express');
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  var buffer = fs.readFileSync('index.html');
-  response.send(buffer.toString());
+ // var fs = require('fs');
+ // var buffer = fs.readFileSync('index.html');
+ // response.send(buffer.toString());
+ response.send("Hello World, Ugh.");
 });
 
 var port = process.env.PORT || 5000;
